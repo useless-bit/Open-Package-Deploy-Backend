a26facf02b82c6021b123c3a4b68c59eb497e502
LucaEchle <luca.echle@codesystem.org>
2024-03-20 06:53:29 +0000

    updated tests

--- agent/src/main/java/org/codesystem/UpdateHandler.java
+++ agent/src/main/java/org/codesystem/UpdateHandler.java
@@ -19,46 +19,11 @@ public class UpdateHandler {
     private final PropertiesLoader propertiesLoader;
     String FILE_NAME_AGENT_UPDATE = "Agent_update.jar";
     Path PATH_UPDATE_FILE = Paths.get(FILE_NAME_AGENT_UPDATE);
-    String FILE_NAME_AGENT_UPDATE_BACKUP = "Agent_backup.jar";
-    Path PATH_BACKUP_FILE = Paths.get(FILE_NAME_AGENT_UPDATE_BACKUP);
-    String FILE_NAME_AGENT = "Agent.jar";
-    Path PATH_FILE = Paths.get(FILE_NAME_AGENT);
 
     public UpdateHandler(DownloadUtility downloadUtility, CryptoHandler cryptoHandler, PropertiesLoader propertiesLoader) {
         this.downloadUtility = downloadUtility;
         this.cryptoHandler = cryptoHandler;
         this.propertiesLoader = propertiesLoader;
-    }
-
-    public void updateApplication() {
-        if (!PATH_UPDATE_FILE.toFile().exists() || !PATH_FILE.toFile().exists()) {
-            throw new SevereAgentErrorException("Cannot find update file");
-        }
-        if (PATH_BACKUP_FILE.toFile().exists()) {
-            try {
-                Files.delete(PATH_BACKUP_FILE);
-            } catch (IOException e) {
-                throw new SevereAgentErrorException("Cannot delete backup: " + e.getMessage());
-            }
-        }
-
-        try {
-            Files.copy(PATH_FILE, PATH_BACKUP_FILE);
-        } catch (IOException e) {
-            throw new SevereAgentErrorException("Cannot create backup of agent: " + e.getMessage());
-        }
-        try {
-            Files.delete(PATH_FILE);
-        } catch (IOException e) {
-            throw new SevereAgentErrorException("Cannot delete old Agent: " + e.getMessage());
-        }
-
-        try {
-            Files.copy(PATH_UPDATE_FILE, PATH_FILE);
-        } catch (IOException e) {
-            throw new SevereAgentErrorException("Cannot copy update: " + e.getMessage());
-        }
-        SystemExit.exit(0);
     }
 
     public void startUpdateProcess(String checksum) {
