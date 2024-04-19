d4bc8e9d19df8b1425ae2395a019f5506ef5236a
LucaEchle <luca.echle@codesystem.org>
2024-04-19 21:29:46 +0000

    added logging

--- server/src/main/java/org/codesystem/server/service/scheduled/DeploymentValidator.java
+++ server/src/main/java/org/codesystem/server/service/scheduled/DeploymentValidator.java
@@ -5,12 +5,14 @@ import org.codesystem.server.entity.AgentEntity;
 import org.codesystem.server.entity.DeploymentEntity;
 import org.codesystem.server.entity.PackageEntity;
 import org.codesystem.server.entity.ServerEntity;
+import org.codesystem.server.enums.log.Severity;
 import org.codesystem.server.repository.*;
 import org.codesystem.server.service.server.LogService;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -31,25 +33,49 @@ public class DeploymentValidator {
     public void validateDeployments() {
         ServerEntity serverEntity = serverRepository.findAll().get(0);
         if (serverEntity.getLastDeploymentValidation() == null || Instant.now().isAfter(serverEntity.getLastDeploymentValidation().plus(serverEntity.getDeploymentValidationInterval(), ChronoUnit.SECONDS))) {
+            logService.addEntry(Severity.INFO, "Deployment Validation started");
+            Instant startTimestamp = Instant.now();
             List<AgentEntity> agentEntities = agentRepository.findAllRegistered();
             List<PackageEntity> packageEntities = packageRepository.findAll();
+
+            long deletedDuplicateDeployments = 0;
+            long createdGroupDeployments = 0;
+            long deletedUnreferencedDeployments = 0;
 
             for (AgentEntity agentEntity : agentEntities) {
                 List<PackageEntity> filteredPackageEntities = packageEntities.stream().filter(entity -> entity.getTargetOperatingSystem().equals(agentEntity.getOperatingSystem())).toList();
                 for (PackageEntity packageEntity : filteredPackageEntities) {
                     if (deploymentRepository.findAllByAgentUUIDAndPackageUUID(agentEntity.getUuid(), packageEntity.getUuid()).size() > 1) {
-                        deleteDuplicateDeployments(agentEntity, packageEntity);
+                        deletedDuplicateDeployments += deleteDuplicateDeployments(agentEntity, packageEntity);
                     }
-                    addMissingDeployment(agentEntity, packageEntity);
-                    removeUnreferencedDeployments(agentEntity, packageEntity);
+                    if (addMissingDeployment(agentEntity, packageEntity)) {
+                        createdGroupDeployments += 1;
+                    }
+                    if (removeUnreferencedDeployments(agentEntity, packageEntity)) {
+                        deletedUnreferencedDeployments += 1;
+                    }
                 }
             }
             serverEntity.setLastDeploymentValidation(Instant.now());
             serverRepository.save(serverEntity);
+            logService.addEntry(Severity.INFO, "Deployment validation finished in: " +
+                    Duration.between(startTimestamp, Instant.now()).getSeconds() +
+                    ". Deleted Duplicate Deployments: " +
+                    deletedDuplicateDeployments +
+                    ". Created Group Deployments: " +
+                    createdGroupDeployments +
+                    ". Deleted Unreferenced Deployments: " +
+                    deletedUnreferencedDeployments
+            );
         }
     }
 
-    private void deleteDuplicateDeployments(AgentEntity agentEntity, PackageEntity packageEntity) {
+    /**
+     * Deletes duplicate deployments.
+     *
+     * @return Returns the amount of duplicate/deleted deployments.
+     */
+    private int deleteDuplicateDeployments(AgentEntity agentEntity, PackageEntity packageEntity) {
         boolean isOneDeploymentDirect = false;
         List<DeploymentEntity> deploymentEntities = deploymentRepository.findAllByAgentUUIDAndPackageUUID(agentEntity.getUuid(), packageEntity.getUuid());
         for (DeploymentEntity deploymentEntity : deploymentEntities) {
@@ -64,26 +90,41 @@ public class DeploymentValidator {
         deploymentEntity.setAgentEntity(agentEntity);
         deploymentEntity.setDirectDeployment(isOneDeploymentDirect);
         deploymentRepository.save(deploymentEntity);
+        return deploymentEntities.size() - 1;
     }
 
-    private void addMissingDeployment(AgentEntity agentEntity, PackageEntity packageEntity) {
+    /**
+     * Creates a missing group deployment.
+     *
+     * @return Returns true if the deployment got created.
+     */
+    private boolean addMissingDeployment(AgentEntity agentEntity, PackageEntity packageEntity) {
         if (!deploymentRepository.isDeploymentAlreadyPresent(agentEntity.getUuid(), packageEntity.getUuid()) && groupRepository.isPackageAvailableThroughGroup(agentEntity.getUuid(), packageEntity.getUuid())) {
             DeploymentEntity deploymentEntity = new DeploymentEntity();
             deploymentEntity.setPackageEntity(packageEntity);
             deploymentEntity.setAgentEntity(agentEntity);
             deploymentEntity.setDirectDeployment(false);
             deploymentRepository.save(deploymentEntity);
+            return true;
         }
+        return false;
     }
 
-    private void removeUnreferencedDeployments(AgentEntity agentEntity, PackageEntity packageEntity) {
+    /**
+     * Deletes unreferenced group deployments.
+     *
+     * @return Returns true if the deployment for the package got deleted.
+     */
+    private boolean removeUnreferencedDeployments(AgentEntity agentEntity, PackageEntity packageEntity) {
         List<DeploymentEntity> deploymentEntities = deploymentRepository.findAllByAgentUUIDAndPackageUUID(agentEntity.getUuid(), packageEntity.getUuid());
         if (deploymentEntities.isEmpty()) {
-            return;
+            return false;
         }
         DeploymentEntity deploymentEntity = deploymentEntities.get(0);
         if (!deploymentEntity.isDirectDeployment() && !groupRepository.isPackageAvailableThroughGroup(agentEntity.getUuid(), packageEntity.getUuid())) {
             deploymentRepository.delete(deploymentEntity);
+            return true;
         }
+        return false;
     }
 }
